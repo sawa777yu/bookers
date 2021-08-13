d0824ce75e4a9ec8557fc719b19d421c6fc5238e
sawa777yu <sawa.oranger7@gmail.com>
2021-08-13 09:08:16 +0000

    [Finish]kadai2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,11 +9,13 @@ class BooksController < ApplicationController
   end
 
   def create
+    @books = Book.all
     @book = Book.new(book_params)
     if @book.save
-     redirect_to book_path(@book.id)
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to book_path(@book)
     else
-      render
+      render :index
     end
   end
 
@@ -26,9 +28,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
   end
 
   def destroy
